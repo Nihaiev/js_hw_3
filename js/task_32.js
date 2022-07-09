24d129777847c399d4dc10917b91d2fe4d163bcd
Иван Нигаев <Nihaiev@it.group>
2022-07-09 17:43:22 +0300

    Update task_32.js

--- js/task_32.js
+++ js/task_32.js
@@ -1,11 +1,18 @@
 // Change code below this line
-function addOverNum(...args) {
+function addOverNum(maxNumber, ...args) {
+  console.log(maxNumber);
+  console.log(args);
   let total = 0;
-
-  for (const arg of args) {
-    total += arg;
+  for (const number of args) {
+    if (number > maxNumber) {
+      total += number;
+    }
   }
-
   return total;
   // Change code above this line
 }
+
+console.log(addOverNum(50, 15, 27));
+console.log(addOverNum(10, 12, 4, 11, 48, 10, 8));
+console.log(addOverNum(15, 32, 6, 13, 19, 8));
+console.log(addOverNum(20, 74, 11, 62, 46, 12, 36));
